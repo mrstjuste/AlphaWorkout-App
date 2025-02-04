dc5791723bd041a0bf025273f04ba14b90ded211
mrstjuste <brianstjuste2021@gmail.com>
2025-02-04 14:41:22 -0500

    Initial Commit

---
+++ AlphaWorkout/AlphaWorkoutApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlphaWorkoutApp.swift
+//  AlphaWorkout
+//
+//  Created by Brian St-Juste on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct AlphaWorkoutApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AlphaWorkout/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AlphaWorkout
+//
+//  Created by Brian St-Juste on 2/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
